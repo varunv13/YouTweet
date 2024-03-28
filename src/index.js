3408a09cc80c7e03c662d3c1822ba6d0d65338e3
varunverma1303@gmail.com <varunverma1303@gmail.com>
2024-03-28 13:25:06 +0530

    updated util code

--- src/index.js
+++ src/index.js
@@ -5,4 +5,18 @@ dotenv.config({
     path: './env'
 });
 
-connectDB();
+connectDB()
+.then(() => {
+
+    app.on("Error", (error) => {
+        console.log("Error: ", error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!", err);
+})
