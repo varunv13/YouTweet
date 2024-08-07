d3a74f9668ca1293d83a2d23f366de805f6cd3cb
varunverma1303@gmail.com <varunverma1303@gmail.com>
2024-08-08 00:06:54 +0530

    created getVideoComment successfully

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -8,7 +8,52 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
-  const { page = 1, limit = 10 } = req.query;
+  const {
+    page = 1,
+    limit = 2,
+    sortBy = "createdAt",
+    sortType = "desc",
+  } = req.query;
+
+  if (!isValidObjectId(videoId)) throw new ApiError(400, "Invalid video id.");
+
+  const pageNumber = parseInt(page, 10);
+  const limitNumber = parseInt(limit, 10);
+
+  const queryObject = {};
+  queryObject[sortBy] = sortType === "asc" ? 1 : -1;
+
+  const comments = await Comment.find()
+    .sort(queryObject)
+    .skip((pageNumber - 1) * limitNumber)
+    .limit(limitNumber);
+
+  if (comments.length === 0) {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, comments, "No comment found"));
+  }
+
+  const totalComments = await Comment.countDocuments();
+  const totalPages = Math.ceil(totalComments / limit);
+  const remainingPages = totalPages - page;
+
+  const pagination = {
+    currentPage: page,
+    totalPages: totalPages,
+    remainingPages: remainingPages,
+  };
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        comments,
+        "Comments fetched Successfully!",
+        pagination,
+      ),
+    );
 });
 
 const addComment = asyncHandler(async (req, res) => {
@@ -42,6 +87,32 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+  const { commentId } = req.params;
+  const { id } = req.user._id;
+  const { content } = req.body;
+
+  if (!isValidObjectId(commentId))
+    throw new ApiError(400, "In-valid comment id");
+
+  const comment = await Comment.findById(commentId);
+  if (!comment) throw new ApiError(404, "Comment does not exist on the video.");
+
+  if (comment.owner.id.toString() !== id.toString()) {
+    throw new ApiError(400, "You are not authorized to make any changes.");
+  }
+
+  if (content?.trim() === "")
+    throw new ApiError(404, "Content of the comment not provided.");
+
+  const updatedComment = await Comment.findOneAndUpdate(
+    comment,
+    { content: content?.trim() },
+    { new: true },
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedComment, "Comment updated successfully"));
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
